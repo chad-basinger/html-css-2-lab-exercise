6b76c8b022400d9548d1ed23409d8996b51aaeb2
Chad Basinger <chad.basingeris@gmail.com>
2021-02-24 13:11:20 -0700

    updated spaceX proj

---
+++ index.js
@@ -0,0 +1,47 @@
+// const headingOne = document.querySelector('h1');
+// // console.log(headingOne)
+
+// const paragraph = document.getElementById('web-title');
+// // console.log(paragraph.innerText);
+// // paragraph.innerText = 'I am a new string';
+// // console.log(paragraph.innerText);
+
+// // const unorderedList = document.querySelector('#unordered-list');
+// paragraph.className = 'some-class'
+
+// //add a class
+// paragraph.classList.add('new-class');
+// console.log(paragraph.className);
+
+// //remove a class
+// paragraph.classList.remove('new-class')
+// console.log(paragraph.className)
+
+// const listItems = document.getElementsByTagName('h2')
+
+// const classItems = document.querySelectorAll('.dragon')
+// // console.log(classItems);
+
+//if one click is causing two events, you prob have a double event(parent, child)
+// document.querySelector('#child').addEventListener('click', function(event){
+//     event.stopPropagation()
+//     console.log('Child button element event invoked')
+// })
+
+function button() {
+    alert("THIS IS SPACE X!!");
+}
+
+function button2(){
+    alert("HOLY SHIP BATMAN!");
+}
+
+function button3(){
+    alert("WELCOME TO THE CREW!");
+}
+
+function button4(){
+    // name = 'Chad';
+    alert("YOU WISH YOU COULD TRY THIS!");
+    // console.log(name);
+}
